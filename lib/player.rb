95218f8c42a6082904054ed6ee0ef599e66b9a2c
Raf Swiderski <rafswid@icloud.com>
2021-11-16 15:51:36 +0000

    passing first test - returning total score

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,12 @@
 class Player
+    def initialize 
+        @total_score = 0
+    end
+
+    def return_total_score
+        @total_score
+    end
+
+
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,10 @@
-require 'total_score.rb'
+require 'player.rb'
 
 describe Player do 
     describe '#total_score' do
+        it 'returns the total score, which has a default of 0' do
+            p1 = Player.new
+            expect(p1.return_total_score).to eq 0
+        end
+    end
 end
