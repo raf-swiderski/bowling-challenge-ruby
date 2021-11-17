580a9ad4592745cc575404f55cfda79c60432af7
Raf Swiderski <rafswid@icloud.com>
2021-11-17 10:25:33 +0000

    test: creates a frame automatically when the roll method is called

--- lib/player.rb
+++ lib/player.rb
@@ -2,16 +2,23 @@ require 'frame.rb'
 
 class Player
 
+    attr_reader :current_frame, :all_frames
+
     def initialize 
         @total_score = 0
+        @frame
     end
 
     def return_total_score
         @total_score
     end
-
+    
     def create_frame
-        @frame = Frame.new
+        @current_frame = Frame.new
+    end
+
+    def roll
+        create_frame
     end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,24 +2,42 @@ require 'player.rb'
 
 describe Player do 
 
-    # before(:each) do
-
-    # end
+    before(:each) do
+        @player = Player.new
+    end
 
 
     describe '#total_score' do
         it 'returns the total score, which has a default of 0' do
-            player = Player.new
-            expect(player.return_total_score).to eq 0
+            
+            expect(@player.return_total_score).to eq 0
         end
     end
 
     describe '#roll' do
 
-        
-        it 'creates a frame which contains the score for that frame' do
-            player = Player.new
-            expect(player.create_frame.score).to eq 0
+        it 'creates a frame automatically when the roll method is called' do
+
+            
+            @player.roll
+            expect(@player.current_frame.score).to eq 0
+
         end
+
+        # it 'receives a random number between 1 & 10 when the #roll method is called for the first time' do
+
+
+
+        # end
+        
+    end
+
+    describe '#create_frame' do
+
+        it 'creates a frame which contains the score for that frame' do
+            
+            expect(@player.create_frame.score).to eq 0
+        end
+
     end
 end
